120122e1bad35ad552c94e9006e5b611b06af3e0
grantposell <grantposell@gmail.com>
2020-12-10 21:41:12 -0600

    Added the landing page framework

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Layout, Header, Navigation, Content, Footer, FooterSection, FooterDropDownSection, FooterLinkList } from 'react-mdl';
+// import Main from "./components/main";
+import { Link } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header className="header-color" title={<Link style={{ textDecoration: 'none', color: 'white' }} to="/">Growologie</Link>} scroll>
+            <Navigation>
+              <Link ripple to="/process">Process</Link>
+              <Link ripple to="/projects">Projects</Link>
+              <Link ripple to="/resume">Resume</Link>
+              <Link ripple to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Content>
+            <div className="page-content" />
+            {/* <Main></Main> */}
+          </Content>
+          <Footer size="mega">
+            <FooterSection type="middle">
+              <FooterDropDownSection title="Features">
+                <FooterLinkList>
+                  <a href="#">About</a>
+                  <a href="#">Terms</a>
+                  <a href="#">Partners</a>
+                  <a href="#">Updates</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Details">
+                <FooterLinkList>
+                  <a href="#">Specs</a>
+                  <a href="#">Tools</a>
+                  <a href="#">Resources</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="Technology">
+                <FooterLinkList>
+                  <a href="#">How it works</a>
+                  <a href="#">Patterns</a>
+                  <a href="#">Usage</a>
+                  <a href="#">Products</a>
+                  <a href="#">Contracts</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+              <FooterDropDownSection title="FAQ">
+                <FooterLinkList>
+                  <a href="#">Questions</a>
+                  <a href="#">Answers</a>
+                  <a href="#">Contact Us</a>
+                </FooterLinkList>
+              </FooterDropDownSection>
+            </FooterSection>
+            <FooterSection type="bottom" logo="Title">
+              <FooterLinkList>
+                <a href="#">Help</a>
+                <a href="#">Privacy & Terms</a>
+              </FooterLinkList>
+            </FooterSection>
+          </Footer>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
