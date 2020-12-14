362e831169a5a0009c2e9e42f2eeb253a26ea2e2
grantposell <grantposell@gmail.com>
2020-12-13 23:48:12 -0600

    Adding landingpage content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Layout, Header, Content, Footer, FooterSection, FooterLinkList } from 'react-mdl';
 import './App.css';
+import Main from "./components/main";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ class App extends Component {
           <Header class="header-color" title={<Link class="link" to="/">Growologie</Link>} scroll>
           </Header>
           <Content style={{ backgroundColor: '#f4f4f4' }}>
+            <Main></Main>
           </Content>
           <Footer size="mini" style={{ backgroundColor: 'black' }}>
             <FooterSection type="left" style={{ color: 'white', }}>
@@ -20,8 +22,8 @@ class App extends Component {
                 <a href="/careers">Careers</a>
                 {/* <a href="/invest">Investors</a> */}
                 {/* <a href="#">Newsroom</a> */}
-                <a href="/privacy">Privacy Policy</a>
-                <a href="/termsofservice">Terms of Service</a>
+                {/* <a href="/privacy">Privacy Policy</a>
+                <a href="/termsofservice">Terms of Service</a> */}
               </FooterLinkList>
             </FooterSection>
           </Footer>
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -3,8 +3,10 @@ import React, { Component } from 'react';
 
 class Landing extends Component {
     render() {
-        return(
-            <div>
+        return (
+            <div style={{ backgroundColor: 'black', height: '400px', }}>
+                <h3 style={{ color: 'white', margin: 0, textAlign: 'center', }}>Coming 2021 </h3><br />
+                <p>One App to manage all procurement</p>
 
             </div>
         )
